ac56e7abf551b4e08719e1fc562f4b8c9970c62a
AlePouroullis <alexpouroullis123@gmail.com>
2022-06-27 00:32:57 +0200

    Finished Gatsby tutorial 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>This is the children prop for the "About Me" page.</p>
+        </Layout>
+    );
+}
+
+export default AboutPage;
